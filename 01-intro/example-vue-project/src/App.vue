447c49f1d20cf864a68677481ce306daecd5b156
kdkaanev <kdkaane@github.io>
2024-11-16 08:28:26 +0200

    new first commit

---
+++ 01-intro/example-vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+
+</script>
+
+<template>
+  <h1>HelloWorld</h1>
+  <HelloWorld />
+</template>
+
---
+++ 02-RenderingAndStyles/project/src/App.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data() {
+    return {
+      isDay: true
+    };
+  },
+  methods:{
+    onDayChange(){
+      this.isDay = !this.isDay
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>HelloWorld</h1>
+  <h2 v-if="isDay">It's sunny outside!</h2>
+  <h2 v-else> It's dadrk
+    outside!</h2>
+
+    <template v-if="isDay">
+  <h1>
+  Welcome to My Awesome Page
+  </h1>
+  <p>
+  Hello there! This is a cool Vue.js
+  example.
+  </p>
+  <p>
+  Let's explore some awesome features
+  together!
+  </p>
+  </template>
+  <p v-else>
+Click the button above to reveal the
+exciting content!
+</p>
+
+    <button @click="onDayChange">
+      Togle time
+    </button>
+<h2 v-show="isDay">
+  THIS SHOULD BE HIDDEN WHEN IS DAY
+</h2>
+
+</template>
+
+
