eafe30b910f31275bfa0ba7656239ac9764c0741
kdkaanev <kdkaane@github.io>
2024-11-28 22:34:48 +0200

    add custom element

--- Forms/start/src/App.vue
+++ Forms/start/src/App.vue
@@ -1,99 +1,30 @@
 <script>
+import AppInput from './components/AppInput.vue';
+import AppNamesInput from './components/AppNamesInput.vue';
+import BasicExample from './components/BasicExample.vue';
+
 export default {
+  components: {
+    BasicExample,
+    AppInput,
+    AppNamesInput,
+  },
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      message: '',
-      frameworks: [],
-      acceptedTerms: 'Accept',
-      car: '',
-      isSelected: false,
-      wasToggled: 'yes',
+      customText: '',
+      fName: 'Kancho',
+      lName: 'Kaanev',
     };
   },
-  methods: {
-    onInput(event) {
-      this.firstName = event.target.value;
-    },
-  },
+
 };
 </script>
 
 <template>
-  <article>
-    <header>
-      Leave your feedback
-    </header>
-    <input type="text" :value="firstName" @input="onInput">
-    <p>My First Name: {{ firstName }}</p>
+  <BasicExample />
 
-    <input v-model="lastName" type="text">
-    <p>My Last Name {{ lastName }}</p>
-
-    <textarea v-model="message" placeholder="Write me a message" />
-    <p>My Message : {{ message }}</p>
-
-    <fieldset>
-      <p>Select your JS framework</p>
-      <label>
-        <input v-model="frameworks" type="checkbox" value="vue">
-        Vue
-      </label>
-      <label>
-        <input v-model="frameworks" type="checkbox" value="txd">
-        React
-      </label>
-      <label>
-        <input v-model="frameworks" type="checkbox" value="ng">
-        Angular
-      </label>
-      <p>Selected :{{ JSON.stringify(frameworks) }}</p>
-    </fieldset>
-
-    <fieldset>
-      Accept the website terms!
-      <label>
-        <input v-model="acceptedTerms" type="radio" value="Accept"> Accept
-      </label>
-      <label>
-        <input v-model="acceptedTerms" type="radio" value="Decline"> Decline
-      </label>
-      <p>Accepted value is: {{ acceptedTerms }}</p>
-    </fieldset>
-    <fieldset>
-      <select v-model="car">
-        <option disabled value="">
-          Please select one
-        </option>
-        <option>Oplel</option>
-        <option>Ferrary</option>
-        <option>Mercedes</option>
-      </select>
-      <p>My car is: {{ car }}</p>
-    </fieldset>
-    <fieldset>
-      <label for="">
-        <input v-model="wasToggled" type="checkbox" true-value="yes" false-value="no">
-        Was I Toggle? <span>{{ wasToggled }}</span>
-      </label>
-    </fieldset>
-    <fieldset>
-      <label for="">
-        <input v-model="isSelected" type="checkbox" value="true">
-        Was I Selected? <span>{{ isSelected }}</span>
-      </label>
-    </fieldset>
-  </article>
+  <AppInput id="example1" v-model="customText" placeholder="Write someting" name="My custom component" />
+  <p>Example: {{ customText }}</p>
+  <AppNamesInput v-model:first-name="fName" v-model:last-name="lName" />
+  <p>My name is: {{ fName }} {{ lName }}</p>
 </template>
-
-<style scoped>
-article {
-  max-width: 500px;
-  margin: 4rem auto 0;
-}
-
-fieldset {
-  border: 1px coral solid;
-}
-</style>
---
+++ Forms/start/src/components/AppNamesInput.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: [
+    'update:firstName',
+    'update:lastName',
+  ],
+};
+</script>
+
+<template>
+  <fieldset>
+    <label for="id">firstName</label>
+    <input id="firstName" type="text" :value="firstName" @input="$emit('update:firstName', $event.target.value)">
+  </fieldset>
+  <fieldset>
+    <label for="id">lastName</label>
+    <input id="lastName" type="text" :value="lastName" @input="$emit('update:lastName', $event.target.value)">
+  </fieldset>
+</template>
