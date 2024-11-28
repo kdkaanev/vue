18b05f7dd03382b0c9fd272ce11eeca853912199
kdkaanev <kdkaane@github.io>
2024-11-28 19:44:54 +0200

    up

---
+++ Forms/start/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      textValue: 'test',
+      title: 'Example title',
+    };
+  },
+  methods: {
+    onInput(event) {
+      this.textValue = event.target.value;
+    },
+  },
+};
+</script>
+
+<template>
+  <input type="text" :value="textValue" @input="onInput">
+  <p>My Text is {{ textValue }}</p>
+
+  <input type="text" :value="title" @input="onInput">
+  <p>My Text is {{ title }}</p>
+</template>
